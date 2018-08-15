80854a555c0f060a165552418e076b8d05902ab6
Schmidt Jonson <fejn@gft.com>
2018-08-15 15:16:52 -0300

    add google login logic

--- app/src/main/java/com/cwb/androiddevbr/cidadaodebem/ui/login/LoginActivity.kt
+++ app/src/main/java/com/cwb/androiddevbr/cidadaodebem/ui/login/LoginActivity.kt
@@ -3,11 +3,21 @@ package com.cwb.androiddevbr.cidadaodebem.ui.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.cwb.androiddevbr.cidadaodebem.R
 import com.cwb.androiddevbr.cidadaodebem.ui.base.BaseActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
@@ -15,9 +25,15 @@ class LoginActivity : BaseActivity() {
     override val activityLayout = R.layout.activity_login
 
     private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val TAG = "LoginActivity"
+
+    private lateinit var gso: GoogleSignInOptions
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 9001
 
     companion object {
-        fun getIntent(context: Context): Intent {
+        fun getIntent (context: Context): Intent {
             return Intent(context, LoginActivity::class.java)
         }
     }
@@ -25,6 +41,77 @@ class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
+        google_login_button.setOnClickListener {
+            signIn()
+        }
     }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.getSignInIntent()
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // [START_EXCLUDE]
+                updateUI(null)
+                // [END_EXCLUDE]
+            }
+
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+        // [START_EXCLUDE silent]
+//        showProgressDialog()
+        // [END_EXCLUDE]
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                         Log.d(TAG, "signInWithCredential:success")
+                        val user = mAuth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        updateUI(null)
+                    }
+
+                    // [START_EXCLUDE]
+//                    hideProgressDialog()
+                    // [END_EXCLUDE]
+                }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+//        hideProgressDialog()
+        if (user != null) {
+            Snackbar.make(root, "logged", Snackbar.LENGTH_SHORT).show()
+            mAuth.signOut()
+        } else {
+            Snackbar.make(root, "not logged", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
 }
